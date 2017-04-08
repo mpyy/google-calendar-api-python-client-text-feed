d9bf7f8f17d4be545a773d7a3bc152dc9795278a
mpyy <matias.pyy@gmail.com>
2017-04-08 19:51:31 +0300

    Initial commit

---
+++ gcalread.py
@@ -0,0 +1,27 @@
+from optparse import OptionParser # in Python 2.7 optparse will be replaced by argparse
+import os.path
+
+GCALFEED_INFILE = 'gcal_feeds.out'
+script_real_path = dirname(realpath(__file__))
+infile = join(script_real_path, GCALFEED_INFILE)
+
+data = []
+
+with open(infile) as f:
+    lines = f.readlines()
+
+for line in lines:
+    data.append(tuple(line.split('\t')))
+
+list.sort(data, key=lambda data: data[0])
+
+parser = OptionParser(description='Print column of data from ' + GCALFEED_INFILE)
+parser.add_option('-c', '--col', type='int', action='store', default=1, help='column(s) to print')
+
+(options, args) = parser.parse_args()
+
+if options.col >= len(data[0]):
+    parser.error("-c option maximum value is %s" % (len(data[0])-1,))
+
+for entry in data:
+    print entry[options.col].strip()
